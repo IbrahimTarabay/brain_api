23b0caf4680ab1e61e8769c2a1fbc18eeba60750
IbrahimTarabay <ibrahimesalah69@gmail.com>
2019-03-09 16:21:04 -0800

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,33 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ /*apiKey: '2347731f43fe4ca38b9bf2daf3bf24d0'*/
+	/*apiKey:'272b823953084132a78c0a6c009781b2'*/
+  apiKey: 'a67248b683de41e1b8fed73773a01efb'
+});
+
+const handleApiCall = (req, res) => {
+	 app.models
+	 .predict(Clarifai.DEMOGRAPHICS_MODEL, req.body.input)
+	 .then(data => {
+	 	res.json(data);
+	 })
+	 .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+const handleImage = (db) => (req,res) => {
+	const {id} = req.body;
+	 db('users').where('id', '=', id)
+     .increment('entries',1)
+     .returning('entries')
+     .then(entries => {
+     	res.json(entries[0]);
+     })
+     .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+	handleImage,
+	handleApiCall
+}
